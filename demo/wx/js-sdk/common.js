249cdc63e46921dd4cb3b8ddbf6bf5fc857eb660
thinkuncle <thinkuncle@gmail.com>
2016-03-22 10:42:25 +0800

    测试2016年3月22日 10:42:19

--- demo/wx/js-sdk/common.js
+++ demo/wx/js-sdk/common.js
@@ -16,10 +16,10 @@ wx.ready(function () {
   // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
   document.querySelector('#onMenuShareAppMessage').onclick = function () {
     wx.onMenuShareAppMessage({
-      title: '互联网之子 方倍工作室',
+      title: '分享测试',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
       link: 'http://movie.douban.com/subject/25785114/',
-      imgUrl: 'http://img3.douban.com/view/movie_poster_cover/spst/public/p2166127561.jpg',
+      imgUrl: 'https://img3.doubanio.com/view/photo/photo/public/p2166127561.jpg',
       trigger: function (res) {
         alert('用户点击发送给朋友');
       },
@@ -33,7 +33,7 @@ wx.ready(function () {
         alert(JSON.stringify(res));
       }
     });
-    alert('已注册获取“发送给朋友”状态事件');
+    alert('发送给朋友事件...');
   };
 
   // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
